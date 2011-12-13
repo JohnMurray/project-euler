c860c5733088b6b6eaeb109308ab62b5a42dd9a8
John Murray <john.m.murray786@gmail.com>
2011-12-12 22:41:02 -0500

    Ruby -- Problem 1
    This is my first pass at problem 1. For the record, I have looked at this
    one before. So, it might not be an entirely fair judge.

--- Ruby/1.rb
+++ Ruby/1.rb
@@ -1,1 +1,14 @@
+#!/usr/bin/env ruby
 
+##----
+## PROBLEM
+##
+## If we list all the natural numbers below 10 that are multiples of 3 or 5, 
+## we get 3, 5, 6 and 9. The sum of these multiples is 23.
+##
+## Find the sum of all the multiples of 3 or 5 below 1000.
+##----
+
+sum = 0
+(1...1_000).each {|x| sum += x if x % 3 == 0 or x % 5 == 0}
+puts sum
